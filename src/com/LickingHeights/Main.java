ddb463314477b0b9291acdec1f14f2dc56a6ddfd
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-03-02 11:55:24 -0500

    IntelliJ Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,21 @@
+package com.LickingHeights;
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("This is");
+            yourName();
+            helloWorld();
+        }
+
+        public static void yourName() {
+
+            System.out.println("James");
+
+        }
+
+        public static void helloWorld() {
+
+            System.out.println("Hello World");
+
+
+        }
+    }
